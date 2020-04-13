cdc084916c2fb15bc401444976948d0029c404be
tiaraarista <tiaraarista99@gmail.com>
2020-04-13 17:46:11 +0700

    First Commit

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour {
+	public bool isEscapeToExit;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			if (isEscapeToExit) {
+				Application.Quit ();
+				Debug.Log("Game is exiting");
+			} 
+			else {
+				Menu ();
+			}
+		}
+	}
+
+	public void Mulai () {
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void Menu () {
+		SceneManager.LoadScene ("Menu");
+	}
+
+	public void Help () {
+		SceneManager.LoadScene ("HowToPlay");
+	}
+
+	public void Credit () {
+		SceneManager.LoadScene ("Credit");
+	}
+
+	public void Quit () {
+		Application.Quit ();
+		Debug.Log("Game is exiting");
+	}
+}
